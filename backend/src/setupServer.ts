ce2a8d5ffae7242705d824d155f8aca3aff0532e
username <790768275@qq.com>
2023-01-01 11:15:10 +0800

    feat:完成securityMiddleware和standardMiddleware

--- backend/src/setupServer.ts
+++ backend/src/setupServer.ts
@@ -1,5 +1,12 @@
 import { Application, json, urlencoded, Response, Request, NextFunction } from 'express'
 import http from 'http'
+import cors from 'cors'
+import helmet from 'helmet'
+import hpp from 'hpp'
+import cookieSession from 'cookie-session'
+import HTTP_STATUS from 'http-status-codes'
+import 'express-async-errors'
+import compression from 'compression'
 
 export class ChattyServer {
     private app: Application;
@@ -17,11 +24,35 @@ export class ChattyServer {
     }
 
     private securityMiddleware(app: Application): void {
-
+        app.use(
+            cookieSession({
+                name: 'session',
+                keys: ['test1', 'test2'],
+                maxAge: 24 * 7 * 360000,
+                secure: false
+            })
+        )
+        app.use(hpp())
+        app.use(helmet())
+        app.use(
+            cors({
+                origin: '*',
+                credentials: true,
+                optionsSuccessStatus: 200,
+                methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+            })
+        )
     }
 
     private standardMiddleware(app: Application): void {
-
+        app.use(compression())
+        app.use(json({
+            limit: '50mb'
+        }))
+        app.use(urlencoded({
+            extended: true,
+            limit: '50mb'
+        }))
     }
 
     private routesMiddleware(app: Application): void {
