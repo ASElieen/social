6539bdae2e0f7dbfa180fec39205230840359bc2
username <790768275@qq.com>
2023-01-03 18:23:26 +0800

    feat:signUp类中搭配authService类和helper类实现创建用户功能

---
+++ backend/src/features/auth/controllers/signUp.ts
@@ -0,0 +1,53 @@
+import { ObjectId } from 'mongodb'
+import { Request, Response } from 'express'
+import { joiValidation } from '@global/decorators/joiValidationDecorator'
+import { signupSchema } from '../schemas/signup'
+import { IAuthDocument, ISignUpData } from '../interfaces/auth.interface'
+import { authService } from '../../../share/services/db/auth.service'
+import { BadRequestError } from '@helpers/errorHandler'
+import { Helpers } from '@global/helpers/helpers'
+import { UploadApiResponse } from 'cloudinary'
+import { uploads } from '@helpers/cloudinaryUpload'
+
+export class SignUp {
+    @joiValidation(signupSchema)
+    public async create(req: Request, res: Response): Promise<void> {
+        const { username, email, password, avatarColor, avatarImage } = req.body
+
+        const checkUserIfExists: IAuthDocument = await authService.getUserByUsernameOrEmail(username, email)
+
+        if (checkUserIfExists) {
+            throw new BadRequestError('无效的请求')
+        }
+
+        const authObjectID: ObjectId = new ObjectId()
+        const userObjectID: ObjectId = new ObjectId()
+        const uId = `${Helpers.generateRandomIntegers(12)}`
+        const authData: IAuthDocument = SignUp.prototype.signUpData({
+            _id: authObjectID,
+            uId,
+            username,
+            email,
+            password,
+            avatarColor
+        })
+        //始终保持publicId为userObjectID，并且不被覆盖
+        const result: UploadApiResponse = await uploads(avatarImage, `${userObjectID}`, true, true) as UploadApiResponse
+        if (!result?.public_id) {
+            throw new BadRequestError('文件上传失败,发生了某些错误,请重试')
+        }
+    }
+
+    private signUpData(data: ISignUpData): IAuthDocument {
+        const { _id, username, uId, email, password, avatarColor } = data
+        return {
+            _id,
+            uId,
+            username: Helpers.firstLetterUppercase(username),
+            email: Helpers.lowerCase(email),
+            password,
+            avatarColor,
+            createdAt: new Date()
+        } as IAuthDocument
+    }
+}
---
+++ backend/src/share/services/db/auth.service.ts
@@ -0,0 +1,18 @@
+import { IAuthDocument } from '../../../features/auth/interfaces/auth.interface'
+import { Helpers } from '../../globals/helpers/helpers'
+import { AuthModel } from '@root/features/auth/models/auth.schema'
+
+
+class AuthService {
+    public async getUserByUsernameOrEmail(username: string, email: string): Promise<IAuthDocument> {
+        //$or在数组表达式的条件下选择至少满足一个条件的文档
+        const query = {
+            $or: [{ username: Helpers.firstLetterUppercase(username) }, { email: Helpers.lowerCase(email) }]
+        }
+
+        const user: IAuthDocument = await AuthModel.findOne(query).exec() as IAuthDocument
+        return user
+    }
+}
+
+export const authService: AuthService = new AuthService()
