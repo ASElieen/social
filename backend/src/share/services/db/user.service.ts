2b5bc24352e1cbe493b269ca15cda757ebc8457b
username <790768275@qq.com>
2023-01-06 16:58:23 +0800

    feat:添加getUserById，可以从mongodb中取出数据

--- backend/src/share/services/db/user.service.ts
+++ backend/src/share/services/db/user.service.ts
@@ -7,6 +7,17 @@ class UserService {
         await UserModel.create(data)
     }
 
+    public async getUserById(userId: string): Promise<IUserDocument> {
+        const users: IUserDocument[] = await UserModel.aggregate([
+            //mongoose类型转换 str=>ObjectId
+            { $match: { _id: new mongoose.Types.ObjectId(userId) } },
+            { $lookup: { from: 'Auth', localField: 'authId', foreignField: '_id', as: 'authId' } },
+            { $unwind: '$authId' },
+            { $project: this.aggregateProject() }
+        ])
+        return users[0]
+    }
+
     public async getUserByAuthId(authId: string): Promise<IUserDocument> {
         const users: IUserDocument[] = await UserModel.aggregate([
             { $match: { authId: new mongoose.Types.ObjectId(authId) } },
@@ -19,7 +30,26 @@ class UserService {
 
     private aggregateProject() {
         return {
-            //todo 返回具体数据
+            _id: 1,
+            username: '$authId.username',
+            uId: '$authId.uId',
+            email: '$authId.email',
+            avatarColor: '$authId.avatarColor',
+            createdAt: '$authId.createdAt',
+            postsCount: 1,
+            work: 1,
+            school: 1,
+            quote: 1,
+            location: 1,
+            blocked: 1,
+            blockedBy: 1,
+            followersCount: 1,
+            followingCount: 1,
+            notifications: 1,
+            social: 1,
+            bgImageVersion: 1,
+            bgImageId: 1,
+            profilePicture: 1,
         }
     }
 }
