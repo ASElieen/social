5d49c69da90f58b7071216bea3f89e3e5f721d19
username <790768275@qq.com>
2023-01-06 15:33:19 +0800

    feat:添加signOut登出功能

--- backend/src/features/auth/routes/authRoutes.ts
+++ backend/src/features/auth/routes/authRoutes.ts
@@ -1,6 +1,7 @@
 import express, { Router } from 'express'
 import { SignUp } from '../controllers/signUp'
 import { SignIn } from '../controllers/signIn'
+import { SignOut } from '../controllers/signOut'
 
 class AuthRoutes {
     private router: Router
@@ -12,6 +13,11 @@ class AuthRoutes {
     public routes(): Router {
         this.router.post('/signup', SignUp.prototype.create)
         this.router.post('/signin', SignIn.prototype.read)
+        return this.router
+    }
+
+    public signOutRoute(): Router {
+        this.router.get('/signout', SignOut.prototype.update)
         return this.router
     }
 }
