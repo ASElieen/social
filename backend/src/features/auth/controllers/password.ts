a140a4f34f9f3f51bea5ac1f13d273bc6058ec15
username <790768275@qq.com>
2023-01-13 11:20:49 +0800

    feat:给忘记密码邮件中的url添加重置密码功能(Password类-update方法)

--- backend/src/features/auth/controllers/password.ts
+++ backend/src/features/auth/controllers/password.ts
@@ -4,11 +4,15 @@ import HTTP_STATUS from 'http-status-codes'
 import { authService } from '@services/db/auth.service'
 import { BadRequestError } from '@global/helpers/errorHandler'
 import { IAuthDocument } from '../interfaces/auth.interface'
-import { emailSchema } from '../schemas/password'
+import { emailSchema, passwordSchema } from '../schemas/password'
 import { joiValidation } from '@global/decorators/joiValidationDecorator'
 import crypto from 'crypto'
 import { forgotPasswordTemplate } from '@services/emails/templates/forgotPassword/forgotPasswordTem'
 import { emailQueue } from '@services/queues/email.queue'
+import { IResetPasswordParams } from '@user/userInterfaces/user.interface'
+import moment from 'moment'
+import publicIP from 'ip'
+import { resetPasswordTemplate } from '@services/emails/templates/resetPassword/resetPasswordTem'
 
 export class Password {
     @joiValidation(emailSchema)
@@ -30,5 +34,39 @@ export class Password {
         emailQueue.addEmailJob('forgotpasswordEmail', { template, receiverEmail: email, subject: '忘记密码' })
 
         res.status(HTTP_STATUS.OK).json({ message: '重置密码邮件已经发送' })
+    }
+
+
+    @joiValidation(passwordSchema)
+    public async update(req: Request, res: Response): Promise<void> {
+        const { password, confirmPassword } = req.body
+        const { token } = req.params
+        if (password !== confirmPassword) {
+            throw new BadRequestError('密码不匹配')
+        }
+
+        const existingUser: IAuthDocument = await authService.getAuthUserByPasswordToken(token)
+        if (!existingUser) {
+            throw new BadRequestError('重置密码token已过期')
+        }
+
+        existingUser.password = password
+        existingUser.passwordResetExpires = undefined
+        existingUser.passwordResetToken = undefined
+        await existingUser.save()
+
+        const templateParams: IResetPasswordParams = {
+            username: existingUser.username,
+            email: existingUser.email,
+            ipaddress: publicIP.address(),
+            date: moment().format('DD//MM//YYYY HH:mm')
+        }
+
+
+        const template: string = resetPasswordTemplate.passwordResetConfirmTemplate(templateParams)
+
+        emailQueue.addEmailJob('forgotpasswordEmail', { template, receiverEmail: existingUser.email, subject: '密码重置' })
+
+        res.status(HTTP_STATUS.OK).json({ message: '密码已成功重置' })
     }
 }
