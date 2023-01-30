c95e53fda780c31496ed32ec8d363428b29f1e75
username <790768275@qq.com>
2023-01-30 11:36:08 +0800

    feat:添加postController和post相关路由

--- backend/src/features/post/routes/postRoutes.ts
+++ backend/src/features/post/routes/postRoutes.ts
@@ -1,6 +1,7 @@
 import express, { Router } from 'express'
 import { authMiddleware } from '@global/helpers/authMiddleware'
 import { Create } from '../controllers/createPost'
+import { Get } from '../controllers/getPosts'
 
 class PostRoutes {
     private router: Router
@@ -12,6 +13,10 @@ class PostRoutes {
     public routes(): Router {
         this.router.post('/post', authMiddleware.checkAuthentication, Create.prototype.post)
         this.router.post('/post/image', authMiddleware.checkAuthentication, Create.prototype.postWithImage)
+
+        //看到所有post或者图片合集
+        this.router.get('/post/all/:page', authMiddleware.checkAuthentication, Get.prototype.posts)
+        this.router.get('/post/images/post', authMiddleware.checkAuthentication, Get.prototype.postsWithImages)
         return this.router
     }
 }
