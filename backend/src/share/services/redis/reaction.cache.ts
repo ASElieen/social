db516e3d7336e52ea8f2a6bc8731a04603bfeff3
username <790768275@qq.com>
2023-02-03 22:49:10 +0800

    feat:添加方法从redis中获取reaction(get)

--- backend/src/share/services/redis/reaction.cache.ts
+++ backend/src/share/services/redis/reaction.cache.ts
@@ -59,6 +59,45 @@ export class ReactionCache extends BaseCache {
         }
     }
 
+    public async getReactionsFromCache(postId: string): Promise<[IReactionDocument[], number]> {
+        try {
+            if (!this.client.isOpen) {
+                await this.client.connect()
+            }
+            //LLEN返回列表的长度
+            const reactionsCount: number = await this.client.LLEN(`reactions:${postId}`)
+            const response: string[] = await this.client.LRANGE(`reactions:${postId}`, 0, -1)
+            const list: IReactionDocument[] = []
+            for (const item of response) {
+                list.push(Helpers.parseJSON(item))
+            }
+            return response.length ? [list, reactionsCount] : [[], 0]
+        } catch (error) {
+            log.error(error)
+            throw new ServerError('从redis中获取reaction失败(get)')
+        }
+    }
+
+    public async getSingleReactionByUsernameFromCache(postId: string, username: string): Promise<[IReactionDocument, number] | []> {
+        try {
+            if (!this.client.isOpen) {
+                await this.client.connect()
+            }
+            const response: string[] = await this.client.LRANGE(`reactions:${postId}`, 0, -1)
+            const list: IReactionDocument[] = []
+            for (const item of response) {
+                list.push(Helpers.parseJSON(item))
+            }
+            const result: IReactionDocument = find(list, (listItem: IReactionDocument) => {
+                return listItem.postId === postId && listItem.username === username
+            }) as IReactionDocument
+            return result ? [result, 1] : []
+        } catch (error) {
+            log.error(error)
+            throw new ServerError('从redis中通过username获取reaction失败(get)')
+        }
+    }
+
     private getPreviousReaction(res: string[], username: string): IReactionDocument | undefined {
         const list: IReactionDocument[] = []
         for (const item of res) {
