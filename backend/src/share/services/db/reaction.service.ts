334cfbbafd8e0abb3453ad72ff48e8538dd897c3
username <790768275@qq.com>
2023-02-03 21:26:46 +0800

    fix:修复mongdb中reaction的_id和post的_id不同导致无法修改reaction

--- backend/src/share/services/db/reaction.service.ts
+++ backend/src/share/services/db/reaction.service.ts
@@ -2,12 +2,20 @@ import { IReactionJob } from '@root/features/reactions/interfaces/reaction.inter
 import { UserCache } from '@services/redis/user.cache'
 import { ReactionModel } from '@root/features/reactions/models/reaction.schema'
 import { PostModel } from '@root/features/post/models/post.schema'
+import { omit } from 'lodash'
+import { IReactionDocument } from '@reaction/interfaces/reaction.interface'
 
 const userCache: UserCache = new UserCache()
 
 class ReactionService {
     public async addReactionDataToDB(reactionData: IReactionJob): Promise<void> {
         const { postId, userTo, userFrom, username, type, previousReaction, reactionObject } = reactionData
+
+        //用lodash的omit删掉reactionObject中的_id防止_id矛盾的问题
+        let updateReactionObject: IReactionDocument = reactionObject as IReactionDocument
+        if (previousReaction) {
+            updateReactionObject = omit(reactionObject, ['_id'])
+        }
 
         const updateReaction = await Promise.all([
             userCache.getUserFromCache(`${userTo}`),
