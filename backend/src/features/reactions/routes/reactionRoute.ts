ce7cab546be68b68e8419c134f127f562934b02d
username <790768275@qq.com>
2023-02-05 10:53:51 +0800

    feat:添加getReactions相关路由

--- backend/src/features/reactions/routes/reactionRoute.ts
+++ backend/src/features/reactions/routes/reactionRoute.ts
@@ -2,6 +2,7 @@ import express, { Router } from 'express'
 import { authMiddleware } from '@global/helpers/authMiddleware'
 import { Add } from '../controllers/addReactions'
 import { Remove } from '../controllers/removeReaction'
+import { GetReaction } from '../controllers/getReactions'
 
 class ReactionRoutes {
     private router: Router
@@ -12,6 +13,10 @@ class ReactionRoutes {
         this.router.post('/post/reaction', authMiddleware.checkAuthentication, Add.prototype.reaction)
 
         this.router.delete('/post/reaction/:postId/:previousReaction:postReaction', authMiddleware.checkAuthentication, Remove.prototype.removeReaction)
+
+        this.router.get('/post/reactions/:postId', authMiddleware.checkAuthentication, GetReaction.prototype.getReactions)
+        this.router.get('/post/single/reaction/username/:username/:postId', authMiddleware.checkAuthentication, GetReaction.prototype.getSingleReactionByUsername)
+        this.router.get('/post/reactions/username/:username', authMiddleware.checkAuthentication, GetReaction.prototype.getAllReactionsByUsername)
 
         return this.router
     }
