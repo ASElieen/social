b6dc0480fdc20b8ccff9f9593b39c24198b081a4
username <790768275@qq.com>
2023-02-05 10:47:38 +0800

    feat:给getReaction controller添加通过用户名获取所有心情的新方法

--- backend/src/features/reactions/controllers/getReactions.ts
+++ backend/src/features/reactions/controllers/getReactions.ts
@@ -8,6 +8,7 @@ import mongoose from 'mongoose'
 const reactionCache: ReactionCache = new ReactionCache()
 
 export class GetReaction {
+    //获取特定post的所有reactions
     public async getReactions(req: Request, res: Response): Promise<void> {
         const { postId } = req.params
 
@@ -18,13 +19,21 @@ export class GetReaction {
         res.status(HTTP_STATUS.OK).json({ message: '获取心情成功', reactions: reactions[0], count: reactions[1] })
     }
 
-    public async singleReactionByUsername(req: Request, res: Response): Promise<void> {
+    //通过username获取一个post中他的reaction
+    public async getSingleReactionByUsername(req: Request, res: Response): Promise<void> {
         const { postId, username } = req.params
 
         const cacheReaction: [IReactionDocument, number] | [] = await reactionCache.getSingleReactionByUsernameFromCache(postId, username)
 
         const reactions: [IReactionDocument, number] | [] = cacheReaction.length ? cacheReaction : await reactionService.getSinglePostReactionsByUsername(postId, username)
 
-        res.status(HTTP_STATUS.OK).json({ message: '获取心情成功', reactions: reactions.length ? reactions[0] : [], count: reactions.length ? reactions[1] : [] })
+        res.status(HTTP_STATUS.OK).json({ message: `获取${username}心情成功`, reactions: reactions.length ? reactions[0] : [], count: reactions.length ? reactions[1] : [] })
+    }
+
+
+    public async getAllReactionsByUsername(req: Request, res: Response): Promise<void> {
+        const { username } = req.params
+        const reactions: IReactionDocument[] = await reactionService.getReactionsByUsername(username)
+        res.status(HTTP_STATUS.OK).json({ message: `获取${username}用户的所有心情数据成功`, reactions })
     }
 }
